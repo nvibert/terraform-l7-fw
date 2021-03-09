ed98c5d5ef2154fa6c26c4ec6ff96201b45ce6d1
Nico Vibert <nvibert@vmware.com>
2021-03-09 19:36:56 +0000

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,1 +1,15 @@
-
+resource "nsxt_policy_context_profile" "test" {
+  display_name = "test"
+  description  = "Terraform provisioned ContextProfile"
+  domain_name {
+    description = "test-domain-name-attribute"
+    value       = ["*-myfiles.sharepoint.com"]
+  }
+  app_id {
+    description = "test-app-id-attribute"
+    value       = ["SSL"]
+    sub_attribute {
+      tls_version = ["SSL_V3"]
+    }
+  }
+}
